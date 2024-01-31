4bc863adf31ebd0e496a46b98ef24007f7ae1395
Aniket Kumar Singh <mailsinghaniket@gmail.com>
2024-01-31 22:44:05 +0530

    initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+ 
+  return (
+    <footer className="bg-dark text-light text-center" >
+    
+    <p className="text-center">
+     Copyright &copy; MyTodosList.com
+
+     </p>
+    </footer>
+  )
+}
